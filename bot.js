ed82baeb3f7638ea29d684a5439134d926d0d016
muzuly <59071056+muzuly@users.noreply.github.com>
2020-08-10 22:05:59 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -345,7 +345,14 @@ client.on('message', message => {
        }
         //write the response value to the channel
      if(response) {
-        message.channel.send(response);
+        message.channel.send(({embed: {
+  color: 3447003,
+  description: "A very simple Embed!"
+}});
+            
+            
+            
+            response);
      }
     }
 });
