e8895c48b7375f5fdc18951ee8acd4d1349030ad
muzuly <59071056+muzuly@users.noreply.github.com>
2021-01-19 18:58:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "NzQxODQyNDk0NTkyOTA5MzQz.Xy9cqQ.Jiq7E1PkZM7JPCfrJKim22fpwGI",
+    token: "NzQxODQyNDk0NTkyOTA5MzQz.Xy9cqQ.0bWYmhLjMfjA4tYBTzeRXYLCr3E",
     channel: "UCSno8tzyog9aYwVFtZmcTNQ",
     messageTemplate: "honestly i really hope i works this time **{author}** **{title}**!\n{url}",
     channel_id: "UCSno8tzyog9aYwVFtZmcTNQ",
