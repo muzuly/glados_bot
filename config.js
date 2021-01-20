94f46568e5cc3fb16e7fdfa7bf2e52288c2ea6a3
muzuly <59071056+muzuly@users.noreply.github.com>
2021-01-19 18:56:18 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "NzQxODQyNDk0NTkyOTA5MzQz.Xy9cqQ.Jiq7E1PkZM7JPCfrJKim22fpwGI",
+    channel: "UCSno8tzyog9aYwVFtZmcTNQ",
+    messageTemplate: "honestly i really hope i works this time **{author}** **{title}**!\n{url}",
+    channel_id: "UCSno8tzyog9aYwVFtZmcTNQ",
+    watchInterval: 30000
+}
