f8b72b6e85d225abc3cf27fafbff43ff913e80cf
Josh Bogen <josh.bogen@agilent.com>
2021-01-22 15:59:56 -0600

    Debugging the youtube code...

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     channel: "UCSno8tzyog9aYwVFtZmcTNQ",
     messageTemplate: "honestly i really hope i works this time **{author}** **{title}**!\n{url}",
     channel_id: "UCSno8tzyog9aYwVFtZmcTNQ",
+    discord_channel_id: "741657541846630542",
     watchInterval: 30000
 }
--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const parser = new (require('rss-parser'))()
+
 client.db = require("quick.db");
-client.request = new (require("rss-parser"))();
+
 client.config = require("./config.js");
 
 client.on("ready", () => {
     console.log("I'm ready also!");
-    handleUploads();
+    checkForUploads();
 });
 
-function handleUploads() {
-    if (client.db.fetch('postedVideos') === null) client.db.set('postedVideos', []);
+function checkForUploads() {
+    if (client.db.fetch('postedVideos') == null) client.db.set('postedVideos', []);
     setInterval(() => {
-        client.request.parseURL('https://www.youtube.com/feeds/videos.xml?channel_id=UCSno8tzyog9aYwVFtZmcTNQ').then((data) => {
+        const url = "https://www.youtube.com/feeds/videos.xml?channel_id=" + client.config.channel_id;
+
+        parser.parseURL(url, function(err, data) {
             if (client.db.fetch('postedVideos').includes(data.items[0].link)) return;
             else {
                 console.log("the bot found a video.");
+
                 client.db.set('videoData', data.items[0]);
-                client.db.push("postedVideos", data.items[0].link);
+                client.db.set('postedVideos', data.items[0].link);
+                let channel = client.channels.cache.get(client.config.discord_channel_id);
                 let parsed = client.db.fetch('videoData');
-                let channel = client.channels.cache.get(client.config.channel);
-                if (!channel) return;
+
                 let message = client.config.messageTemplate
                     .replace(/{author}/g, parsed.author)
                     .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
                     .replace(/{url}/g, parsed.link);
                 channel.send(message);
             }
-        });
+
+        })
     }, client.config.watchInterval);
 }
 
-client.login(client.config.token);
+client.login(process.env.BOT_TOKEN);
+
+741657541846630542
