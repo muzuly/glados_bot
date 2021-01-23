4d86b4d86368ca7b5871e405657b9e7a653d1878
muzuly <59071056+muzuly@users.noreply.github.com>
2021-01-23 17:37:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	token: process.env.BOT_TOKEN,
-    channel: "UCSno8tzyog9aYwVFtZmcTNQ",
+    channel: "UCfyAXfto41vsC2i-QCFiqgw",
     messageTemplate: "**{author}** is here to bring you your **{title}**!\n{url}",
-    channel_id: "UCSno8tzyog9aYwVFtZmcTNQ",
+    channel_id: "UCfyAXfto41vsC2i-QCFiqgw",
     discord_channel_id: "741657541846630542",
     watchInterval: 30000
 }
