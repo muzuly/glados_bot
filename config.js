c0ab63a75edd88a5bccefac0133cbc12b2c24232
muzuly <59071056+muzuly@users.noreply.github.com>
2021-01-23 17:32:44 -0600

    Changed the message and maybe something else

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	token: process.env.BOT_TOKEN,
     channel: "UCSno8tzyog9aYwVFtZmcTNQ",
-    messageTemplate: "honestly i really hope i works this time **{author}** **{title}**!\n{url}",
+    messageTemplate: "**{author}** is here to bring you your **{title}**!\n{url}",
     channel_id: "UCSno8tzyog9aYwVFtZmcTNQ",
     discord_channel_id: "741657541846630542",
     watchInterval: 30000
