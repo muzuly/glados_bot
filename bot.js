6563edda928998dc78acdf2eeba377dd36de11a6
muzuly <59071056+muzuly@users.noreply.github.com>
2021-01-31 00:41:23 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,7 @@ hashMap.set('chair', "One of the basic pieces of furniture, a chair is a type of
 hashMap.set('foo', 'Bar!');
 hashMap.set('ping', 'Pong!');
 hashMap.set('test', 'Pass!');
+hashMap.set('suckmynuts', 'I mean... :embarrassed:');
 
 
 
